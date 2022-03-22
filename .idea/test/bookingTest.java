0a8f756a975abd6b7c1392590100c418a1bfd8e9
GeorginaWalsh <74904371+GeorginaWalsh@users.noreply.github.com>
2022-03-22 15:14:58 +0000

    Update bookingTest.java

--- .idea/test/bookingTest.java
+++ .idea/test/bookingTest.java
@@ -5,6 +5,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class bookingTest {
 
+<<<<<<< HEAD
+    private Booking booking1, booking2;
+
+        /*
+                * Method to set up data for testing.
+         */
+    @BeforeEach
+    void setUp() {
+        booking1 = new Booking("ry100", "Nov 12", "12:00", "Library", 5);
+        booking2 = new Booking("lelio0", "Jan 26", "13:45", "CMLC", 1);
+    }
+
+        /*
+                * Test method for Booking constructor
+=======
     private booking booking1, booking2;
 
         /**
@@ -18,10 +33,50 @@ public class bookingTest {
 
         /**
          * Test method for Booking constructor
+>>>>>>> origin/main
          */
-        @Test
-        public void testConstructor() {
+    @Test
+    public void testConstructor() {
 
+<<<<<<< HEAD
+        assertEquals("ry100",  booking1.getName());
+        assertEquals("Nov 12", booking1.getDate());
+        assertEquals("12:00", booking1.getTime());
+        assertEquals("Library", booking1.getLocation());
+        assertEquals(5, booking1.getGroup());
+
+    }
+
+    /**
+     * Test method for getter methods
+     */
+
+    @Test
+    public void getName(){
+        assertEquals("lelio0", booking2.getName());
+    }
+
+    @Test
+    public void getDate(){
+        assertEquals("Jan 26", booking2.getDate());
+    }
+
+    @Test
+    public void getTime(){
+        assertEquals("13:45", booking2.getTime());
+    }
+
+    @Test
+    public void getLocation(){
+        assertEquals("CMLC", booking2.getLocation());
+    }
+
+    @Test
+    public void getGroup(){
+        assertEquals(1, booking2.getGroup());
+    }
+}
+=======
             assertEquals("ry100",  booking1.getName());
             assertEquals("Nov 12", booking1.getDate());
             assertEquals("12:00", booking1.getTime());
@@ -60,3 +115,4 @@ public class bookingTest {
         }
 }
 
+>>>>>>> origin/main
